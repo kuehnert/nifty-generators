35197cdaa67a35e55679af39b425a7c8577f403c
Matthias Kühnert <m.kuehnert@mac.com>
2010-08-20 14:04:00 +0200

    Using respond_with and formtastic

--- rails_generators/nifty_scaffold/templates/actions/update.rb
+++ rails_generators/nifty_scaffold/templates/actions/update.rb
@@ -2,8 +2,6 @@
     @<%= singular_name %> = <%= class_name %>.find(params[:id])
     if @<%= singular_name %>.update_attributes(params[:<%= singular_name %>])
       flash[:notice] = "Successfully updated <%= name.underscore.humanize.downcase %>."
-      redirect_to <%= item_path('url') %>
-    else
-      render :action => 'edit'
     end
+    respond_with @<%= singular_name %>
   end
--- rails_generators/nifty_scaffold/templates/controller.rb
+++ rails_generators/nifty_scaffold/templates/controller.rb
@@ -1,3 +1,5 @@
 class <%= plural_class_name %>Controller < ApplicationController
+  respond_to :html, :xml
+
   <%= controller_methods :actions %>
 end
